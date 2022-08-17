8fcc770186f320f7323a69389d8d8ec4ea41946e
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-17 22:34:41 +0300

    commit tree 518a215a262837f0a4893dd201fc007ec517a302

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,14 @@
 class Book
-    attr_reader :title, :author, :page_count, :genre
+    attr_reader :title
 
-    def initialize(title)
+    attr_accessor :author, :page_count, :genre
+    def initialize(title, turn_page="Flipping the page...wow, you read fast!")
         @title = title
         @author = author
-        @page_count = page_count
-        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
     end
 end
 
