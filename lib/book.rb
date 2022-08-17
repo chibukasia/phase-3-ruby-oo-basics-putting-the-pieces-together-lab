9dfb6b575b3af2554ff99bbe60bb1836ff5e3675
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-17 21:55:36 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+    attr_reader :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+end
 
 
